dbfc4bbc44301d0934c73969e1d740960fc6fd62
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-01-31 13:52:50 +0530

    Practice_Programme

---
+++ Addition,Substraction,Multiplication,Divide/01 Addition of Two Num .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int num1=0,num2=0,sum=0;
+
+printf("enter num1:");
+scanf("%d",&num1);
+
+printf("enter num2:");
+scanf("%d",&num2);
+
+ sum=num1+num2;
+
+printf("sum is %d",sum);
+getch();
+
+return 0;
+}
