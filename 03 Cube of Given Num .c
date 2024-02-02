2486d1b3dd761b3eadca72c1ccff09dfdd0e6493
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-02 19:09:54 +0530

    Practice example

---
+++ 03 Cube of Given Num .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+
+int main()
+{
+int num=0;
+int cube=0;
+
+printf("enter num:");
+scanf("%d",&num);
+
+cube=num*num*num;
+
+printf("cube is %d",cube);
+getch();
+
+return 0;
+}
