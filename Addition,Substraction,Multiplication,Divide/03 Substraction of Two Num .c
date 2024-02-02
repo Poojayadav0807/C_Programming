43bc6ce791ff3700f2696cfc432730cbeb2fbbe6
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-02 19:02:18 +0530

    Practice example

---
+++ Addition,Substraction,Multiplication,Divide/03 Substraction of Two Num .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int num1=0,num2=0,sub=0;
+
+printf("enter num1:");
+scanf("%d",&num1);
+
+printf("enter num2:");
+scanf("%d",&num2);
+
+ sub=num1-num2;
+
+printf("sum is %d",sub);
+getch();
+
+return 0;
+}
