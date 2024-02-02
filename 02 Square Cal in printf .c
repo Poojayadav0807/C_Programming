89fa9d3e46c079b3f6b828c85ca5ed3d497a6144
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-02 19:08:32 +0530

    Practice example

---
+++ 02 Square Cal in printf .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+
+int main()
+{
+int num=0;
+int sqrt=0;
+
+printf("enter num:");
+scanf("%d",&num);
+
+
+
+printf("sqrt is %d",(sqrt=num*num),sqrt);
+getch();
+
+return 0;
+}
