058adaf4b5556c206a04606605d790e33af77e81
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-03 21:36:07 +0530

    Practice example

---
+++ Array/01 Printing array.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int num[5]={};
+
+  printf("\nEnter 1st num =%d",num[0]);
+  printf("\nEnter 2nd num =%d",num[1]);
+  printf("\nEnter 3rd num =%d",num[2]);
+  printf("\nEnter 4th num =%d",num[3]);
+  printf("\nEnter 5th num =%d",num[4]);
+
+
+  getch();
+
+  printf("\n enter 5 array element :");
+
+
+  printf("\nenter 1st num :");
+  scanf("%d",&num[0]);
+
+  printf("\nenter 2nd num :");
+  scanf("%d",&num[1]);
+
+  printf("\nenter 3rd num :");
+  scanf("%d",&num[2]);
+
+  printf("\nenter 4th num :");
+  scanf("%d",&num[3]);
+
+  printf("\nenter 5th num :");
+  scanf("%d",&num[4]);
+
+
+  getch();
+
+
+
+  printf("given array element :");
+
+
+  printf("\n num 1st=%d",num[0]);
+  printf("\n num 2nd=%d",num[1]);
+  printf("\n num 3rd=%d",num[2]);
+  printf("\n num 4th=%d",num[3]);
+  printf("\n num 5th=%d",num[4]);
+
+
+  getch();
+  return 0;
+
+
+
+}
