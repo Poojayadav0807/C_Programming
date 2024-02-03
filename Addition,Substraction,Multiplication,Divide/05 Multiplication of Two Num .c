c500998f867267170d6e348500e5c396bec691c8
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-03 23:19:43 +0530

    Practice

---
+++ Addition,Substraction,Multiplication,Divide/05 Multiplication of Two Num .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int num1=0,num2=0,mul=0;
+
+printf("enter num1:");
+scanf("%d",&num1);
+
+printf("enter num2:");
+scanf("%d",&num2);
+
+ mul=num1*num2;
+
+printf("sum is %d",mul);
+getch();
+
+return 0;
+}
