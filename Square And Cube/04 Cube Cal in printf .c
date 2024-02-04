41440525c519f8b84761f9e8b1624fc1ff92b225
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-04 20:23:36 +0530

    Practice example

---
+++ Square And Cube/04 Cube Cal in printf .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+
+int main()
+{
+int num=0;
+int cube=0;
+
+printf("enter num:");
+scanf("%d",&num);
+
+
+printf("cube is %d",(cube=num*num*num),cube);
+getch();
+
+return 0;
+}
