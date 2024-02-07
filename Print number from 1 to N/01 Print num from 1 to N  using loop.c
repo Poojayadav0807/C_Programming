9e53120206e3d940c5ca7b3ac636434b857d82c5
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-07 22:57:03 +0530

    Practice

---
+++ Print number from 1 to N/01 Print num from 1 to N  using loop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    int i=0,n=0;
+
+    printf("\n Enter value of n =");
+    scanf("%d",&n);
+
+    for(i=0;i<=n;i++)
+    {
+    printf("\n%d",i);
+
+    }
+    return 0;
+}
