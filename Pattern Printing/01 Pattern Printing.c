e985b55695fbdf2ac1face91a2273e492c89509d
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-09 19:38:09 +0530

    Practicw

---
+++ Pattern Printing/01 Pattern Printing.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+int i=0,j=0,r=0,c=0;
+
+for(i=1;i<=5;i++)
+{
+for(j=1;j<=7;j++)
+{
+
+printf(" * ");
+}
+printf("\n");
+}
+
+getch();
+return 0;
+}
