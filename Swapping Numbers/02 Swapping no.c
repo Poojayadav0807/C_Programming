f852e41c56706f3a85a6103eb33fea98f4e23a34
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-11 21:07:46 +0530

    Practice

---
+++ Swapping Numbers/02 Swapping no.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num1=0,num2=0;
+
+printf("enter num1 :");
+scanf("%d",&num1);
+printf("enter num2 :");
+scanf("%d",&num2);
+
+printf("\n\nnumber before swapping %d %d", num1,num2);
+printf("\n\nnumber after swapping:%d %d",num2 ,num1);
+
+
+
+getch();
+return 0;
+}
