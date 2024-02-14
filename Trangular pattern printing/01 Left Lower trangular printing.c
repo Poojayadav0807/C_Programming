1cc0657c780bf3cd58a2328248ad8f1fad4e7c2a
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-14 21:40:44 +0530

    Practice

---
+++ Trangular pattern printing/01 Left Lower trangular printing.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+ int i=0,j=0,x=0,num=2;
+
+    printf("\n Enter no of rows and columns :");
+    scanf("%d",&x);
+
+
+    for(i=1; i<=x;i++)
+    {
+        for(num=2,j=1;j<=x;j++)
+        {
+            if(i>=j)
+            {
+              printf("%2d ",num);
+              num=num+2;
+            }
+
+        }
+        printf("\n");
+    }
+   getch();
+   return 0;
+
+}
