416b3b3aed67864778da94e1183e0d9fd6c50353
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-15 22:17:13 +0530

    Practice

---
+++ Trangular pattern printing/08 Left lower triangular 0 to 10 printing.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0,j=0,count=0;
+
+    for(i=0;i<=5;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+
+            printf("%d",count);
+            count++;
+
+        }
+
+        printf("\n");
+
+    }
+
+
+return 0;
+
+}
