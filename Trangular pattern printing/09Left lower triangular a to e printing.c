58053aa15ff9aec9d0eb22cfa3cb658cc82cf377
Poojayadav0807 <poojajyadav0807@gmail.com>
2024-02-15 22:17:53 +0530

    Practice

---
+++ Trangular pattern printing/09Left lower triangular a to e printing.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0,j=0;
+
+    for(i=97;i<=101;i++)
+    {
+        for(j=97;j<=i;j++)
+        {
+
+            printf("%c",j);
+
+
+        }
+
+        printf("\n");
+
+    }
+
+
+return 0;
+
+}
